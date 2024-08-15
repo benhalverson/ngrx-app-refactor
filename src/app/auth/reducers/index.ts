d1cd3f080ea8fa555ce903e3dffa1325fe5318c6
Ben Halverson <benhalverson33@gmail.com>
2024-08-15 01:17:43 -0700

    query store without selectors

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -3,17 +3,34 @@ import {
   ActionReducer,
   ActionReducerMap,
   createFeatureSelector,
+  createReducer,
   createSelector,
-  MetaReducer
+  MetaReducer,
+  on,
+  State
 } from '@ngrx/store';
+import { User } from '../model/user.model';
+import { AuthActions } from '../action.types';
 
 export interface AppState {
 
 }
 
-export const reducers: ActionReducerMap<AppState> = {
+export interface AuthState {
+  user: User;
 
+}
+
+export const initialAuthState: AuthState = {
+  user: {
+    id: '',
+    email: ''
+  }
 };
 
 
-export const metaReducers: MetaReducer<AppState>[] = isDevMode() ? [] : [];
+export const authReducer = createReducer(initialAuthState, on(AuthActions.login, (state, action) => {
+  return {
+    user: action.user
+  }
+}));
